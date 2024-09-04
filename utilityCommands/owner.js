2368ba0ac5775220e7ff438103c5fca521364f0d
Krishnaexe <bugaliavishnu99990@gmail.com>
2024-09-04 23:09:44 +0530

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -4,12 +4,12 @@ module.exports = {
     name: 'owner',
     description: 'Bot owner info',
     execute(message, args) {
-        const youtubeLink = 'https://www.youtube.com/@GlaceYT';
-        const InstagramLink = 'https://www.instagram.com/glaceytt';
+        const DiscordLink = 'https://www.youtube.com/@GlaceYT';
+        const InstagramLink = 'https://www.instagram.com/krishna__exe_?igsh=Y2U2eWRocjY5MGMy';
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
             .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [GlceYT](${youtubeLink})\n 💙 [GlaceYTT](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n 🤖 Myself Krishna . I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [krishna](${youtubeLink})\n 💙 [GlaceYTT](${InstagramLink})`)
             .setTimestamp();
 
 
